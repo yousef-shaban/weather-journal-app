4cef93f06d14e1ead7d9b7fc6ccf3524113a8259
yousef-shaban <joe.kapo123@gmail.com>
2021-12-04 01:43:58 +0200

    install the node and express and add the getData func and the postData func and remains the updata the UI func

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [
+    
+];
+
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser')
 
 // Start up an instance of app
-
+const app = express()
+const port = 3000;
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// listining function
+const listining = ()=>{console.log("server lisining on port: " + port)}
 
 // Setup Server
+app.listen(port, listining)
+
+// routes
+
+app.get("/weatherData", (req, res)=>
+{
+    res.send(projectData)
+});
+
+app.post("/addData", (req, res)=>
+{
+    let data = req.body;
+
+    projectData.push(data)
+
+});
+
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,68 @@
 /* Global Variables */
+const api = '760849b3c2e8c48e77e846d6f63b2be5'
+const Gen = document.getElementById('generate');
+let WeatherData = {}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+Gen.addEventListener('click', preform)
+
+
+
+// the main function
+function preform(){
+
+    getData()
+    .then((data)=>{
+
+        postData("/addData", WeatherData)
+        updataUI();
+    })
+}
+
+
+
+const getData = async ()=>
+    {
+        const zip = document.getElementById('zip').value;
+        const text = document.getElementById("feelings").value;
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${api}`
+        const Rowdata = await fetch(url);
+    
+        try{
+            const data = await Rowdata.json()
+            WeatherData = {
+                temp: data.main.temp,
+                data: newDate,
+                userRes: text
+            }
+            return WeatherData;
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
+
+
+// post the data to the server
+const postData = async (url = '', data = {})=>
+{
+    const post = await fetch(url,
+    
+    {method: 'POST',credentials: "same-origin" ,headers: {'Content-Type': 'application/json',} ,body: JSON.stringify(data)});
+    try{
+        console.log("data posted")
+    }
+    catch(error)
+    {
+        console.log(error)
+    }
+}
+
+// updata the UI function
+function updataUI(){
+    
+}
